36e4576ee4ea828c5f26fa87d2e40a3ad4f02d83
Doug Badger <dougbadger@hotmail.com>
2024-09-04 17:13:36 +0100

    feat: developing frontend

--- public/index.js
+++ public/index.js
@@ -5,22 +5,85 @@ const speechText = document.getElementById("cow-speech");
 const noticeboard = document.getElementById("noticeboard");
 const moo = document.getElementById("moo")
 const cowbell = document.getElementById("cowbell")
+const cowpat = document.getElementById("cowpat")
 const clouds = document.querySelectorAll(".cloud")
-let whatTheCowSays = "Moooooooo!"
+const whatTheCowSays = ["Moooooooo!", "Hey, who turned off all the lights?<br>Brrr, I'm freisian!", "Oh, What a beautiful Mooooorning!", "Hey, quit poking me, that's mooolestation!", "Oi, don't make me hoof it over there - you'll udderly regret it!"]
 
-function theCowSpeaks(whatTheCowSays) {
+// average uk supermarket price per litre (aug 2024)
+const milkPrices = {
+  cow: 0.95,
+  oat: 2,
+  soya: 2,
+  almond: 2
+}
+
+console.log("Hello")
+fetch('./cowquotes.json')
+.then((res) => {console.log(res.json())})
+
+// frontend interaction
+
+function theCowSpeaks(mooing) {
   speechBubble.style.opacity = "1";
   moo.play();
-  speechText.innerText = whatTheCowSays
+  speechText.innerHTML = mooing
   setTimeout(()=>{
     speechBubble.style.opacity = "0";
-    speechText.innerText = "";
-  }, "5000")
+    speechText.innerHTML = "";
+  }, "10000")
 }
+
+let cowPokeCount = 0;
+function pokeTheCow() {
+  if (cowPokeCount>2) {
+    theCowSpeaks(whatTheCowSays[cowPokeCount])
+  } else {
+    theCowSpeaks(whatTheCowSays[0])
+  }
+  cowPokeCount++
+  return cowPokeCount>4 ? cowPokeCount = 0 : cowPokeCount
+}
+
+// button functions
 
 function buttonSound() {
   cowbell.currentTime = 0;
   cowbell.play();
+}
+
+function buttonOneClick() {
+  buttonSound()
+}
+
+function buttonTwoClick() {
+  buttonSound()
+}
+
+function buttonThreeClick() {
+  buttonSound()
+}
+
+// tab functions
+
+function tabSound() {
+  cowpat.currentTime = 0;
+  cowpat.play();
+}
+
+function tabOneClick() {
+  tabSound()
+}
+
+function tabTwoClick() {
+  tabSound()
+}
+
+function tabThreeClick() {
+  tabSound()
+}
+
+function tabFourClick() {
+  tabSound()
 }
 
 function dayNight() {
@@ -31,7 +94,7 @@ function dayNight() {
     noticeboard.style.backgroundImage = "url(images/dark-noticeboard.png)";
     noticeboard.style.color = "white";
     speechText.style.color = "white";
-    theCowSpeaks("Hey, who turned off all the lights?");
+    theCowSpeaks(whatTheCowSays[1]);
   } else {
     cowImage.src = "images/daycow.png";
     clouds.forEach(cloud => cloud.style.backgroundColor = "hsla(0, 0%, 100%, 0.9)");
@@ -39,13 +102,11 @@ function dayNight() {
     noticeboard.style.backgroundImage = "url(images/noticeboard.png)";
     speechText.style.color = "black";
     noticeboard.style.color = "black";
-    theCowSpeaks("What a beautiful sunny day! Mooooo!");
+    theCowSpeaks(whatTheCowSays[2]);
   }
 }
 
-/* this needs to happen on successful login 
-theCowSpeaks(welcomeUser)
-*/
+function buyMilk() {
 
-let thusSpakeTheCow = whatTheCowSays
+}
 
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
   });
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
